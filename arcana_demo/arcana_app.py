d92c37f80ca4895c100f62392d2076dba55885b1
rarconaut <68246130+rarconaut@users.noreply.github.com>
2021-02-16 11:37:36 -0800

    Home page is functional. Working on Ingestre and Tixall pages.

--- arcana_demo/arcana_app.py
+++ arcana_demo/arcana_app.py
@@ -1,59 +1,17 @@
 import os
-from forms import AddForm, DelForm
-from flask import Flask, render_template, session, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask import Flask, render_template, session, url_for, request, redirect
 
 
+#################################################
+# Flask Setup
+#################################################
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'mysecretkey'
 
-# Database
-basedir = os.path.abspath(os.path.dirname(__file__))
-# heroku_database_url = 'postgres://tutdwubbdbjifp:08e0b6d5f87f94b7881b70ccb4e97925ac07a62e5b9040121532d6e6f2dccda6@ec2-34-230-167-186.compute-1.amazonaws.com:5432/d1523ss5ad4q8a'
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@localhost:5432/job_changer"
-# app.config['SQLALCHEMY_DATABASE_URI'] = heroku_database_url
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-db = SQLAlchemy(app)
-Migrate(app, db)
-
-#######################
-# Models
-#######################
-
-class Person(db.Model):
-
-    __tablename__ = 'response'
-
-    id = db.Column(db.Integer, primary_key=True)
-    gender = db.Column(db.Text)
-    relevant_experience = db.Column(db.Text)
-    education_level = db.Column(db.Text)
-    major_discipline = db.Column(db.Text)
-    company_type = db.Column(db.Text)
-    company_size = db.Column(db.Text)
-    
-    def __init__(self,gender,relevant_experience,education_level,major_discipline,company_type,company_size):
-        self.gender = gender
-        self.relevant_experience = relevant_experience
-        self.education_level = education_level
-        self.major_discipline = major_discipline
-        self.company_type = company_type
-        self.company_size = company_size
-        
-    def __repr__(self):
-        return f'Gender: {self.gender}, Experience: {self.relevant_experience}, Education: {self.education_level}, Major: {self.major_discipline}, ' \
-               f'Industry: {self.company_type}, Company Size: {self.company_size}'
-
-    
-######
+#################################################
 # View functions
+#################################################
 
 @app.route('/')
 def index():
@@ -101,25 +59,6 @@ def list_person():
 
     return render_template('list.html', person=person)
 
-@app.route('/predict')
-def Model_Prediction():
-    # gender = Person.query.all()
-    # Gender
-    # gender = str(gender[-1]).split()[1].replace(',','')
-    # Experience
-    # experience = Person.query.all()
-    # experience = str(experience[-1]).split()[3:6]
-    # experience = ','.join(experience).replace(',',' ')
-    
-    X = 'Are you likely to change jobs: '
-    
-    # load model
-    # model = joblib.load("HR_LRmodel_trained_V2.h5")
-    # # Survey Predictions
-    # prediction = model.predict(X)
-    # print(f"First 10 Predictions:   {prediction}")
-    # print("Model: " + model.__class__.__name__)
-    return render_template('predict.html', X=X)
 
 @app.route('/delete', methods=['GET', 'POST'])
 def delete_person():
